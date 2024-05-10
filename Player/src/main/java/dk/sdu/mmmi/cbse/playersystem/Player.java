3d49802ca9b33869ef9ce776a960bc6099d7acc3
Ahmed <1ahmed.deeq@gmail.com>
2024-05-10 12:58:40 +0200

    Behaviour of the bullets are as intended

--- Player/src/main/java/dk/sdu/mmmi/cbse/playersystem/Player.java
+++ Player/src/main/java/dk/sdu/mmmi/cbse/playersystem/Player.java
@@ -1,6 +1,12 @@
 package dk.sdu.mmmi.cbse.playersystem;
 
+import dk.sdu.mmmi.cbse.common.bullet.Bullet;
 import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.World;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -8,4 +14,35 @@ import dk.sdu.mmmi.cbse.common.data.Entity;
  */
 public class Player extends Entity {
 
+    private long bulletFiredTime;
+    private int timesHit;
+    private List<Bullet> bullets = new ArrayList<>();
+
+    public long getBulletFiredTime() {
+        return bulletFiredTime;
+    }
+
+    public void setBulletFiredTime(long bulletFiredTime) {
+        this.bulletFiredTime = bulletFiredTime;
+    }
+
+    @Override
+    public void handleCollision(GameData gameData, World world, Entity collidingEntity) {
+        if (collidingEntity instanceof Bullet) {
+            if (timesHit < 3) {
+                timesHit++;
+            } else {
+                world.removeEntity(this);
+            }
+            world.removeEntity(collidingEntity);
+        }
+    }
+
+    public List<Bullet> getBullets() {
+        return bullets;
+    }
+
+    public void addBullet(Bullet bullet) {
+        bullets.add(bullet);
+    }
 }
