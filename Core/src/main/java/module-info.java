33236807e2b0c2522259e6115263efaeb58adc02
Ahmed <1ahmed.deeq@gmail.com>
2024-05-12 20:49:21 +0200

    Implemented Spring in Core module.

--- Core/src/main/java/module-info.java
+++ Core/src/main/java/module-info.java
@@ -1,8 +1,13 @@
 module Core {
     requires Common;
-    requires CommonBullet;    
-    requires javafx.graphics;
+    requires CommonBullet;
     requires Enemy;
+    requires javafx.graphics;
+    requires spring.context;
+    requires spring.core;
+    requires spring.beans;
+    exports dk.sdu.mmmi.cbse.main to javafx.graphics,spring.core; ;
+
     opens dk.sdu.mmmi.cbse.main to javafx.graphics;
     uses dk.sdu.mmmi.cbse.common.services.IGamePluginService;
     uses dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
